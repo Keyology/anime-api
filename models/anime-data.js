5327bc56eacce5fbb484407c349d9fe2582853af
Keoni Murray <keonimurray45@gmail.com>
2018-12-04 13:03:36 -0800

    created anime model

---
+++ models/anime-data.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+animeshowsSchema = new Schema({
+
+    _id: mongoose.Schema.Types.ObjectId,
+    anime_id: Number,
+    name: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    genre: {
+        type: String
+
+    },
+    type: String,
+
+    episode: Number,
+    rating: Number,
+    members: Number
+
+
+})
