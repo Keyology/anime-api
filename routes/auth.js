c17c14307e21b1bbc554fcac3d1af0e51efc889c
Keoni Murray <keonimurray45@gmail.com>
2018-12-06 02:52:30 -0800

    authentication complete

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const User = require('../models/user')
 
 
@@ -62,6 +62,53 @@ module.exports = (app) => {
 
     app.post('/login', (req, res) => {
         // this route will login users
+
+        User.findOne({
+                //checks database to see if email is a match
+                email: req.body.email
+            })
+            .exec()
+            .then(user => {
+                //decrypts password in database and compares password user submit to find match 
+                bcrypt.compare(req.body.password, user.password, (err, result) => {
+                    if (err) {
+                        //if the email or password dose't match throw error 
+                        return res.status(401).json({
+                            failed: 'Unauthorized Acess'
+                        });
+                    }
+                    if (result) {
+                        //if password and email exist in database create a jwt token
+                        let token = jwt.sign({
+                            id: user.Account_id
+                        }, process.env.SECRET, {
+                            expiresIn: "60 days"
+                        });
+
+
+
+
+                        res.cookie('nToken', token, {
+                            maxAge: 900000,
+                            httpOnly: true
+                        });
+
+                        console.log(req.cookies);
+                        res.redirect('/')
+                        console.log(user);
+                    }
+
+
+
+                });
+            }).catch(error => {
+                //handles error on server side
+                res.status(500).json({
+                    error: "hitting catch"
+                });
+                console.log('this is an error:', error)
+            });
+
 
     })
 }
